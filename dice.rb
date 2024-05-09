3adea8634a188eb71df52952d992996c9655122d
emilyluzhang <55498035+emilyluzhang@users.noreply.github.com>
2024-05-09 23:10:56 +0000

    2

--- dice.rb
+++ dice.rb
@@ -38,11 +38,11 @@ get("/dice/2/10") do
 	erb(:two_ten)
 end
 get("/dice/1/20") do
-  first_die = rand(1..20)
-  sum = first_die #+ second_die
-	
-  @outcome = "You rolled a #{first_die} for a total of #{sum}." #and a #{second_die} for a total of #{sum}."
-	erb(:one_twenty)
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
 end
 get("/dice/5/4") do
   first_die = rand(1..6)
@@ -54,4 +54,15 @@ get("/dice/5/4") do
 	
   @outcome = "You rolled a #{first_die}, #{second_die},  #{third_die},  #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 	erb(:five_four)
+end
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
